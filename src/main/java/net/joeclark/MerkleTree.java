290f88dc0a56a47c0719e1f1264de39ce0ffd7ad
Joe Clark <joeclark.phd@gmail.com>
2024-12-30 15:32:24 -0500

    implement merkle tree insertion + search

--- src/main/java/net/joeclark/MerkleTree.java
+++ src/main/java/net/joeclark/MerkleTree.java
@@ -1,9 +1,14 @@
 package net.joeclark;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * This class implements a Merkle Tree (either the root, or a node in the tree)
- * as an unbalanced binary search tree where each leaf node holds a piece of data
- * and its SHA3 hash, and every root or branch node holds a hash of the two hashes
+ * as an unbalanced binary search tree where each leaf node holds the SHA3 hash
+ * of a piece of data, and every root or branch node holds a hash of the two hashes
  * beneath it.
  * 
  * Given a piece of data, we can hash it and very quickly find if that data's hash
@@ -12,6 +17,91 @@ package net.joeclark;
  */
 public class MerkleTree {
 
+    private MerkleTree left;
+    private MerkleTree right;
+    private byte[] hash;
 
+    public MerkleTree(String data) throws Exception {
+        this.hash = HashHelper.hash256(data);
+    }
+
+    private MerkleTree(byte[] hash) {
+        this.hash = hash;
+    }
+
+    /**
+     * Checks if the data's hash is stored in the tree.
+     * 
+     * Don't misunderstand: the data itself is not stored in the Merkle tree, only its hash,
+     * as we assume it is practically infeasible to come up with altered 'data' that gives
+     * the same hash.
+     */
+    public boolean contains(String data) throws Exception {
+        byte[] newHash = HashHelper.hash256(data);
+        return this.containsHash(newHash);
+    }
+
+    private boolean containsHash(byte[] newHash) {
+        if(isLeaf()) {
+            return Arrays.equals(newHash, this.hash);
+        } else {
+            return left.containsHash(newHash) || right.containsHash(newHash);
+        }
+    }
+
+    private boolean isLeaf() {
+        return left==null && right==null;
+    }
+
+    /**
+     * Inserts the hash of the new data into the tree.
+     * This is an "unbalanced" binary search tree, not the most sophisticated algorithm.
+     * 
+     * Note: the data itself is not stored, just its hash, which is enough to verify it
+     * if we assume it's infeasible for a malicious actor or a malfunction to come up
+     * with different 'data' that produces the same output when hashed.
+     */
+    public void insert(String data) throws Exception {
+        byte[] newHash = HashHelper.hash256(data);
+        insertHash(newHash);
+    }
+
+    private void insertHash(byte[] newHash) throws IOException {
+        if(isLeaf()) {
+            // if we're a leaf node, create two new leaves and turn this into a branch node with a hash of the two hashes
+            if(new BigInteger(newHash).compareTo(new BigInteger(hash))<0) {
+                left = new MerkleTree(newHash);
+                right = new MerkleTree(hash);
+                hash = hashTogether(newHash,hash);
+            } else {
+                left = new MerkleTree(hash);
+                right = new MerkleTree(newHash);
+                hash = hashTogether(hash, newHash);
+            }
+        } else {
+            // we're not a leaf node. pass the hash down the tree
+            if(new BigInteger(newHash).compareTo(new BigInteger(right.minHash()))<0) { // todo: easier way to compare hashes?
+                left.insertHash(newHash);
+            } else {
+                right.insertHash(newHash);
+            }
+        }
+    }
+
+    private byte[] minHash() {
+        if(isLeaf()) {
+            return this.hash;
+        } else {
+            return left.minHash();
+        }
+    }
+
+    private byte[] hashTogether(byte[] leftHash, byte[] rightHash) throws IOException {
+        ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+        baoStream.write(leftHash);
+        baoStream.write(rightHash);
+        byte[] combinedHashes = baoStream.toByteArray();
+        return HashHelper.hash256(combinedHashes);
+    }
 
 }
--- src/test/java/net/joeclark/MerkleTreeTest.java
+++ src/test/java/net/joeclark/MerkleTreeTest.java
@@ -1,5 +1,6 @@
 package net.joeclark;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,25 @@ public class MerkleTreeTest
 {
     /** Test that tests are running normally I guess. */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    private MerkleTree generateTestTree() throws Exception {
+        MerkleTree tree = new MerkleTree("alpha");
+        tree.insert("beta");
+        tree.insert("gamma");
+        tree.insert("delta");
+        tree.insert("epsilon");
+        return tree;
+    }
+
+    @Test
+    public void testMerkleTreeContains() throws Exception {
+        MerkleTree tree = generateTestTree();
+        assertTrue(tree.contains("delta"));
+        assertFalse(tree.contains("omega"));
+    }
+
+
 }
